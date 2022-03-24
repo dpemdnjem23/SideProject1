52023b993df6fc9a84e9aa00bc16f0f713f87d8b
gitkrak <dpemdnjem23@gmail.com>
2022-03-24 23:35:06 +0900

    시작

--- server/index.js
+++ server/index.js
@@ -1,6 +1,25 @@
 const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const app = express();
+const Port = 5000;
+
+app.use(express.json()); //미들웨어
+app.use(express.urlencoded({ extended: false })); //미들웨어
+app.use(cookieParser()); // 미들웨어
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello Nodes.js");
+});
+
+app.listen(Port, () => {
+  console.log(`Server is Startin on ${Port}`);
 });
