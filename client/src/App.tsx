879bbb6f3f8fe5d460cd7965e8cc244f75af28d6
gitkrak <dpemdnjem23@gmail.com>
2022-04-12 23:20:36 +0900

    menu bar 작성중

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,9 +3,11 @@ import { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
 import logo from "./logo.svg";
 import Modal from "./Components/common/modal/signinmodal";
+import "./css/reset.css";
 
 import "./App.css";
-const App: FC <any> = () => {
+import Mainheader from "Components/common/mainHeader";
+const App = () => {
   const [showModal, setShowModal] = useState<boolean>(false);
   //modal을 클릭했을때 modal창이 뜯
   const openmodal = () => {
@@ -17,6 +19,7 @@ const App: FC <any> = () => {
       {showModal ? <Modal setShowModal={setShowModal} /> : null}
 
       <header className="App-header">
+        <Mainheader />
         <button onClick={openmodal}>modal</button>
 
         <div className="App-mainHeader">{/* 로그인 모달창 */}</div>
--- client/src/Components/common/mainHeader.tsx
+++ client/src/Components/common/mainHeader.tsx
@@ -1,2 +1,25 @@
-import React from 'react';
+import React from "react";
+import "../../css/common/mainHeader.css";
+//왜 tsx는 FC를 넣을까?
+//
+const Mainheader = () => {
+  //main header 에 들어갈것
+  //로그인(로그아웃), 메인 페이지 , 구독 모음, 개인정보(회원탈퇴), 구독 공유, 구독 달력
 
+  return (
+    <div id="header">
+      <div className="main_menu">
+        <span className="logo">subgather</span>
+        <ul>
+          <li className="menu">구독모음</li>
+          <li className="menu">구독달력</li>
+          <li className="menu">로그인/회원가입</li>
+          <li className="menu">메인페이지</li>
+          <li className="menu">개인정보</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Mainheader;
