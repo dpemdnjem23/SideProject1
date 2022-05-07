1ce5853ac9764ea6a134502174ee7f877b1331f2
gitkrak <dpemdnjem23@gmail.com>
2022-05-07 22:16:19 +0900

    modal

--- client/src/Components/Modal/signModal.tsx
+++ client/src/Components/Modal/signModal.tsx
@@ -1,42 +1,45 @@
 import React, { useState } from "react";
 
 import "../../css/common/modal/signinmodal.css";
-import "../../css/reset.css"
+import "../../css/reset.css";
 const SigninModal: React.FC<any> = ({ setShowModal }) => {
   const closemodal = () => {
     setShowModal(false);
-
   };
   return (
     <div onClick={closemodal} className="modal">
-    
-      <section onClick={(e) => e.stopPropagation()} >
+      <section onClick={(e) => e.stopPropagation()}>
         <header>
           <button onClick={closemodal} className="close">
             X
           </button>
         </header>
-        <div className="modal-image">간편가입 / 로그인</div>
-        <div className ="modal-image-bottom">내가 무엇을 구독했는지 관리해보세요!</div>
-        <div className="modal-login-input">
+        <div className="modal_title">간편가입 / 로그인</div>
+        <div className="modal_title_desc">
+          내가 무엇을 구독했는지 관리해보세요!
+        </div>
+        <div className="modal_login_section">
           <input type="text" name="username" placeholder="이메일"></input>
-          <div className="modal-id"></div>
+          <div className="modal_id"></div>
+          <input
+            type="password"
+            name="password"
+            placeholder="비밀번호(문자,숫자포함 8자 이상)"
+          ></input>
+          <div className="modal_password"></div>
         </div>
 
-        <div className="modal-login-input">
-          <input type="password" name="password" placeholder="비밀번호(문자,숫자포함 8자 이상)"></input>
-          <div className="modal-password"></div>
+        <div className="modal_login-_button">
+          <span className="email_login_bt">
+            <span className="email_login_ic">로그인</span>
+          </span>
+          <span className="google_login_bt">
+            <span className="google_login_ic">구글계정으로 계속하기</span>
+          </span>
+          <span className="kakao_login_bt">
+            <span className="kakao_login_ic">카카오계정으로 계속하기</span>
+          </span>
         </div>
-
-        <div className="modal-login-button">
-          <span className="email_login_bt">로그인</span>
-          <span className='facebook_login_bt'></span>
-          <span className='kakao_login_bt'></span>
-   
-          
-        </div>
-
-     
       </section>
     </div>
   );
