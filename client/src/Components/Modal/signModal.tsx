dd85c48dee51acf5222b484cad4737c1f7dd3e13
gitkrak <dpemdnjem23@gmail.com>
2022-05-10 23:01:39 +0900

    css

--- client/src/Components/Modal/signModal.tsx
+++ client/src/Components/Modal/signModal.tsx
@@ -20,30 +20,33 @@ const SigninModal: React.FC<any> = ({ setShowModal }) => {
           </div>
         </header>
         <div className="modal_login_section">
-         
-            <input type="text" name="username" placeholder="이메일"></input>
+          <input type="text" name="username" placeholder="이메일"></input>
 
-            <input
-              type="password"
-              name="password"
-              placeholder="비밀번호(문자,숫자포함 8자 이상)"
-            ></input>
-          </div>
-      
+          <input
+            type="password"
+            name="password"
+            placeholder="비밀번호(문자,숫자포함 8자 이상)"
+          ></input>
+        </div>
 
         <div className="modal_button">
           <div className="email_login_bt">
-            <span className="email_login_ic">로그인</span>
+            <span className="email_login ic"></span>
+            로그인
+            
           </div>
-          <span className="signup_bt">
-            <span className="signup_ic">이메일계정으로 회원가입</span>
-          </span>
-          <span className="google_login_bt">
-            <span className="google_login_ic">구글계정으로 계속하기</span>
-          </span>
-          <span className="kakao_login_bt">
-            <span className="kakao_login_ic">카카오계정으로 계속하기</span>
-          </span>
+          <div className="signup_bt">
+            <span className="signup ic"></span>
+            이메일계정으로 회원가입
+          </div>
+          <div className="google_login_bt">
+            <span className="google_login ic"></span>
+            구글계정으로 계속하기
+          </div>
+          <div className="kakao_login_bt">
+            <span className="kakao_login ic"></span>
+            카카오계정으로 계속하기
+          </div>
         </div>
       </section>
     </div>
