680e178567b7cea05179bb6fcfe70b1d6b6468df
gitkrak <dpemdnjem23@gmail.com>
2022-05-24 22:52:43 +0900

    component 수정

---
+++ client/src/Components/mypageComponent/mypageSub.tsx
@@ -0,0 +1,29 @@
+import react from "react";
+
+const MypageSub = () => {
+  return (
+
+
+
+    <div className="Mypage_1 sub">
+              <div className="Mypage_1 textarea"></div>
+              <div className="Mypage_1 textarea">
+                <span className="text_top">구독 등록</span>
+                <span className="text_middle">나만의 구독을 등록해주세요!</span>
+              
+                  <span className ='text_regist1 bottom'>구독 등록 </span>
+                
+              </div>
+            </div>
+            <div className="Mypage_2 sub">
+              <div className="Mypage_2 textarea">
+                <span className='text_top'>구독 모음</span>
+                <span className='text_middle'>나의 구독을 글과 함께 공유해요!</span>
+                
+                  <span className ='text_regist2 bottom'>구독 모음  </span>
+              
+              </div>
+            </div>
+  );
+};
+export default MypageSub
--- client/src/Pages/MyPage.tsx
+++ client/src/Pages/MyPage.tsx
@@ -7,37 +7,12 @@ const MyPage = () => {
   return (
     <div id="Mypage">
       <div className="Mypage_section">
-        <div className="Mypage_left section">
-          <div className="Mypage_user">
-            dfgsdfgd
-            <span className="Mypage_user_text">내 정보</span>
-          </div>
-          <span className="Mypage_sub_text">나의 구독</span>
-          <div className="Mypage_sub_section">
-            <div className="Mypage_1 sub">
-              <div className="Mypage_1_textarea">
-                <span>나만의 구독을 등록해주세요</span>
-                <span>구독 등록</span>
-                <span>구독 등록 하러 가기 </span>
-              </div>
-            </div>
-            <div className="Mypage_2 sub">
-              <div className='Mypage_2_textarea'>
-              <span>구독 모음</span>
-              <span>나의 구독을 글과 함께 공유해요!</span>
-              <div>
-                <span>구독 모음 등록하러 가기</span>
-              </div>
+       
 
-              </div>
             
-            </div>
           </div>
         </div>
-        <div className="Mypage_right section"></div>
-      </div>
-    </div>
-  );
+     
 };
 
 export default MyPage;
