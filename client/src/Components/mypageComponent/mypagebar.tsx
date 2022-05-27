830be9d496cb0a7c589ac46e155de3d35c907b97
gitkrak <dpemdnjem23@gmail.com>
2022-05-27 23:00:58 +0900

    mypage bar

--- client/src/Components/mypageComponent/mypagebar.tsx
+++ client/src/Components/mypageComponent/mypagebar.tsx
@@ -1,25 +1,33 @@
-import React from "react";
+import React,{useRef} from "react";
 import "../../css/components/MyPage/mypagebar.css";
 
 const Mypagebar = () => {
+
+  const line = useRef()
   //현재 결제가 남은 구독갯수
 
   //top에는 결제일이 3일남앗다 3일남은 구독이 몇갡지
   //bottom에는 개인정보 수정
   //center에는 남은 결제액 / 총액수
 
+  //span의 변화가 일어나면 위쪽의 css도변화를 준다.
+
   return (
     <>
       <div className="Mypage_bar_top container">
-        <div className="Mypage_bar_top_date">
-          <span>섭개더 x월</span>
-          <div>
-            <span>결제일 </span>
-            <span>결제금액</span>
-          </div>
-
-          {/* <span>3-days / 2 (결제금액)</span> */}
+        <div className="Mypage_bar_top_title">구독 결제일 / 결제금액</div>
+<div className='Mypage_bar_top_section'>
+        <div className="Mypage_bar_top_section1">
+          <span className="Mypage_bar_top_section1_days">결제일 </span>
+          <span ref={line} className="Mypage_bar_top_section1_pay">결제금액</span>
         </div>
+        <div className="Mypage_bar_top_section2">
+          <span className="Mypage_bar_top_section2_days">14 일</span>
+          <span className="Mypage_bar_top_section2_slash">/</span>
+          <span className="Mypage_bar_top_section2_pay">5,000 원</span>
+        </div>
+        </div>
+        {/* <span>3-days / 2 (결제금액)</span> */}
       </div>
       <div className="Mypage_bar_gap"></div>
       <div className="Mypage_bar_center container">
