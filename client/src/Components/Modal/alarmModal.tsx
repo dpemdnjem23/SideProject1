1ce2127482144badd7f79ef003494aa576d0262d
gitkrak <dpemdnjem23@gmail.com>
2022-09-09 23:50:13 +0900

    알림

--- client/src/Components/Modal/alarmModal.tsx
+++ client/src/Components/Modal/alarmModal.tsx
@@ -22,42 +22,37 @@ const AlarmModal: React.FC<alarmModal> = ({ closeAlarmModal }) => {
   // useEffect(()=>{
 
   // },[])
-const handlebulkReadClick = () =>{
-
-
-  //버튼 클릭시 alarm에 존재하는 read update
-  axios
-    .patch(
-      `${process.env.REACT_APP_API_URI}/alarm/update`,
-      // { id: el },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          authorization: `Bearer ${accessToken}`,
-        },
-      }
-    )
-    .then((res) => {
-      console.log(res)
-
-    //   axios
-    //     .get(`${process.env.REACT_APP_API_URI}/alarm/info`, {
-    //       headers: {
-    //         authorization: `Bearer ${accessToken}`,
-    //       },
-    //     })
-    //     .then((res) => {
-    //       setAlarmInfo(res.data.data);
-    //     })
-    //     .catch((err) => {
-    //       console.log(err);
-    //     });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-
-}
+  const handlebulkReadClick = () => {
+    //버튼 클릭시 alarm에 존재하는 read update
+    axios
+      .patch(
+        `${process.env.REACT_APP_API_URI}/alarm/update`,
+        {},
+        {
+          headers: {
+            "Content-Type": "application/json",
+            authorization: `Bearer ${accessToken}`,
+          },
+        }
+      )
+      .then(() => {
+        axios
+          .get(`${process.env.REACT_APP_API_URI}/alarm/info`, {
+            headers: {
+              authorization: `Bearer ${accessToken}`,
+            },
+          })
+          .then((res) => {
+            setAlarmInfo(res.data.data);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   const handleReadClick = (el: number) => {
     console.log(el);
@@ -72,7 +67,7 @@ const handlebulkReadClick = () =>{
           },
         }
       )
-      .then((res) => {
+      .then(() => {
         // console.log(res)
 
         axios
@@ -101,9 +96,16 @@ const handlebulkReadClick = () =>{
     setAlarmMode(true);
   };
 
+  //알람을 가져와서 표현을할때에 존재하지 않으면,
+
+  // <h1>현재 도착한 새알림이 없습니다.</h1>
+  // <p>
+  //   현재 구독 에 대한 알림만 진행하고 있습니다.<br></br>
+  //   새로운 구독 정보나 갱신이 되면 알려드리겠습니다.
+  // </p>
   const isSelected = alarmMode ? "newAlarm" : "oldAlarm";
   const isSelected2 = alarmMode ? "oldAlarm" : "newAlarm";
-  const isSelectedAlarm = alarmInfo.length <= 0 ? "" : "newTextBox";
+  // const isSelectedAlarm = alarmInfo.length <= 0 ? "" : "newTextBox";
 
   return (
     <div onClick={() => closeAlarmModal()} id="AlarmModal">
@@ -131,61 +133,69 @@ const handlebulkReadClick = () =>{
               </div>
 
               <div className="AlarmModal_header_contents">
-                <div onClick = {handlebulkReadClick} className="AlarmModal_header_contents_read">모두 읽음 처리</div>
+                <div
+                  onClick={handlebulkReadClick}
+                  className="AlarmModal_header_contents_read"
+                >
+                  모두 읽음 처리
+                </div>
               </div>
             </div>
 
             <div className="AlarmModal_section_body">
               {alarmMode ? (
-                alarmInfo.map((el) => {
-                  if (el.read === true) {
-                    return (
-                      <div className="alarmInfo_old" key={el.id}>
-                        <div className="alarmInfo_Section1">
-                          <div className="alarmInfo_Section1_imgarea">
-                            <img src={el.image} />
-                          </div>
-                          <div className="alarmInfo_Section1_contentarea">
-                            <div>
-                              <div className="alarmInfo_Section1_contentarea_title">
-                                {el.title}
-                              </div>
-                            </div>
+                <div className="AlarmModal_section_body_text_old ">
+                  알림은 7일 동안 보관됩니다.
+                </div>
+              ) : (
+                <div className={`AlarmModal_section_body_textbox`}>
+                  <h1>현재 도착한 새알림이 없습니다.</h1>
+                  <p>
+                    현재 구독 에 대한 알림만 진행하고 있습니다.
+                    <br></br>
+                    새로운 구독 정보나 갱신이 되면 알려드리겠습니다.
+                  </p>
+                </div>
+              )}
 
-                            <div>
-                              갱신까지 {el.remain_time}일 남았습니다.
-                              확인해주세요{" "}
+              {alarmMode
+                ? alarmInfo.map((el) => {
+                    if (el.read === true) {
+                      return (
+                        <div className="alarmInfo_old" key={el.id}>
+                          <div className="alarmInfo_Section1">
+                            <div className="alarmInfo_Section1_imgarea">
+                              <img src={el.image} />
                             </div>
-                            {/* <div className="alarmInfo_Section1_contentarea_date"></div> */}
-                          </div>
-                          <div className="alarmInfo_Section1_contentarea2">
-                            <span>
-                              {el.createdAt.substr(5, 2) +
-                                "월 " +
-                                el.createdAt.substr(8, 2) +
-                                "일"}
-                            </span>
+                            <div className="alarmInfo_Section1_contentarea">
+                              <div>
+                                <div className="alarmInfo_Section1_contentarea_title">
+                                  {el.title}
+                                </div>
+                              </div>
+
+                              <div>
+                                갱신까지 {el.remain_time}일 남았습니다.
+                                확인해주세요{" "}
+                              </div>
+                              {/* <div className="alarmInfo_Section1_contentarea_date"></div> */}
+                            </div>
+                            <div className="alarmInfo_Section1_contentarea2">
+                              <span>
+                                {el.createdAt.substr(5, 2) +
+                                  "월 " +
+                                  el.createdAt.substr(8, 2) +
+                                  "일"}
+                              </span>
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    );
-                  }
-                })
-              ) : (
-                <div
-                  className={`AlarmModal_section_body_textbox ${isSelectedAlarm}`}
-                >
-                  {alarmInfo.length <= 0 ? (
-                    <>
-                      <h1>현재 도착한 새알림이 없습니다.</h1>
-                      <p>
-                        현재 구독 에 대한 알림만 진행하고 있습니다.<br></br>
-                        새로운 구독 정보나 갱신이 되면 알려드리겠습니다.
-                      </p>
-                    </>
-                  ) : (
-                    alarmInfo.map((el) => {
-                      if (el.read === false) {
+                      );
+                    }
+                  })
+                : alarmInfo.map((el) => {
+                    if (el.read === false) {
+                      if (el) {
                         return (
                           <div className="alarmInfo" key={el.id}>
                             <div className="alarmInfo_Section1">
@@ -219,11 +229,22 @@ const handlebulkReadClick = () =>{
                             </div>
                           </div>
                         );
+                      } else {
+                        console.log(el);
+                        return (
+                          <div className={`AlarmModal_section_body_textbox`}>
+                            {/* {alarmInfo.length <= 0 ? ( */}
+                            <h1>현재 도착한 새알림이 없습니다.</h1>
+                            <p>
+                              현재 구독 에 대한 알림만 진행하고 있습니다.
+                              <br></br>
+                              새로운 구독 정보나 갱신이 되면 알려드리겠습니다.
+                            </p>
+                          </div>
+                        );
                       }
-                    })
-                  )}
-                </div>
-              )}
+                    }
+                  })}
             </div>
           </div>
         </div>
