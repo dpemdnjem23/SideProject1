bab6a17c9a2a27caaefdca33997b6c04296ce98b
gitkrak <dpemdnjem23@gmail.com>
2022-10-11 17:46:15 +0900

    구독 모음 관리

---
+++ server/seeders/20221010133829-share.js
@@ -0,0 +1,445 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("shares", [
+      {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡"] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+      ,
+
+      {
+        id: 2,
+        title: "난 이게 좋더라...",
+        description:
+          "이제 추천안해요~ ",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", "해피문데이"] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 3,
+        title: "전 요즘 이거봐요...",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        id: 1,
+        title: "무서운 이야기",
+        description:
+          "제가 어제 말했던 리스트 입니다. 참고해주시기 바랍니다 전 이렇게가 좋더라구요",
+        list_sub: { list_sub: ["넷플릭스", "현대셀렉션", "쿠팡", ""] },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },ㅍ
+      
+    ]);
+
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("shares", null, {});
+
+    /**
+     *
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
