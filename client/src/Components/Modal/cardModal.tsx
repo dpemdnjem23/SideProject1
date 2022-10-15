985898bd4a0768eb9c16b217f1cc8b5e8d9ea294
gitkrak <dpemdnjem23@gmail.com>
2022-10-15 23:45:10 +0900

    cardmodal

--- client/src/Components/Modal/cardModal.tsx
+++ client/src/Components/Modal/cardModal.tsx
@@ -1,20 +1,36 @@
-import React from 'react'
+import React from "react";
+import { shareCarduseStore } from "utils/state";
 
-import '../../css/common/modal/cardModal.css'
-const CardModal = () =>{
+import "../../css/common/modal/cardModal.css";
+const CardModal = () => {
+  const { shareInfo, cardIndex, clickModalNum, setCardModal } =
+    shareCarduseStore();
 
-    return(
-        <div id="CardModal">
-            <div className='CardModal_section'>
-                
-            </div>
+  console.log(shareInfo[clickModalNum].list_sub);
 
-
+  return (
+    <div id="CardModal">
+      <div onClick={(e) => e.stopPropagation()} className="CardModal_section">
+        <div className="CardModal_section_Xbutton">
+          <button onClick={() => setCardModal(false)}>X</button>
         </div>
-    )
+        <div className="CardModal_section_top">
+          <div className="CardModal_section_top_title">
+            {shareInfo[clickModalNum].title}
+          </div>
+          <div className="CardModal_section_top_nick">
+            작성자: {shareInfo[clickModalNum].user.nickname}
+          </div>
+        </div>
+        <div className="CardModal_section_center">
+          <div>{shareInfo[clickModalNum].description}</div>
+        </div>
+        <div>
+          <div>{shareInfo[clickModalNum].list_sub}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-
-
-}
-
-export default CardModal
+export default CardModal;
