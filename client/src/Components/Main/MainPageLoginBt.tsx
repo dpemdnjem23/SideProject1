92b8c0cd3c239768b14f774d4351b802bfde9fe1
gitkrak <dpemdnjem23@gmail.com>
2022-11-01 23:53:24 +0900

    s

--- client/src/Components/Main/MainPageLoginBt.tsx
+++ client/src/Components/Main/MainPageLoginBt.tsx
@@ -14,38 +14,40 @@ import { faSnowflake } from "@fortawesome/free-regular-svg-icons";
 const MainPageLoginBt = () => {
   const options = {
     root: null, // .container class를 가진 엘리먼트를 root로 설정. null일 경우 브라우저 viewport
-    rootMargin: "10px", // rootMargin을 '10px 10px 10px 10px'로 설정
-    threshold: [0, 0.5, 1], // 타겟 엘리먼트가 교차영역에 진입했을 때, 교차영역에 타켓 엘리먼트의 50%가 있을 때, 교차 영역에 타켓 엘리먼트의 100%가 있을 때 observe가 반응한다.
+    rootMargin: "0px", // rootMargin을 '10px 10px 10px 10px'로 설정
+    threshold: 1 // 타겟 엘리먼트가 교차영역에 진입했을 때, 교차영역에 타켓 엘리먼트의 50%가 있을 때, 교차 영역에 타켓 엘리먼트의 100%가 있을 때 observe가 반응한다.
   };
 
   const io = new IntersectionObserver((entries, observer) => {
       // entry와 observer 출력
       entries.forEach((entry) => {
 
-        if(entry.intersectionRatio>0){
-          entry.target.classList.remove('zoom-out')
-
+        if(entry.isIntersecting){
+console.log(entry.isIntersecting)
             entry.target.classList.add("zoom-in")
           
         }
         else{
-          entry.target.classList.remove('zoom-in')
 
+          // entry.target.classList.remove('MainPageLoginBt')
             entry.target.classList.add('zoom-out')
         }
-        console.log(observer,'observer')
       
         
       });
   }, options);
 
-  const button:any = document.querySelector('.MainPageLoginBt')
+  const button:any = document.querySelectorAll('.MainPageLoginBt')
+
+  button.forEach((el:any)=>{
+    io.observe(el)
+  })
 
   // io.observe(button)
 
     // io.observe(button);
      return (
-    <div className="MainPageLoginBt zoom-in zoom-out">
+    <div className="MainPageLoginBt">
       <Link to="/login">
         <FontAwesomeIcon icon={faSnowflake} /> 섭개더 시작하기
       </Link>
