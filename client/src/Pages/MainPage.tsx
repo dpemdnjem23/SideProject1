ec7c5aee10ff96455190b2852f83bb4ca44e46aa
gitkrak <dpemdnjem23@gmail.com>
2022-11-04 23:52:40 +0900

    수정

--- client/src/Pages/MainPage.tsx
+++ client/src/Pages/MainPage.tsx
@@ -15,7 +15,7 @@ const MainPage = () => {
 
   const options = {
     root: null, // .container class를 가진 엘리먼트를 root로 설정. null일 경우 브라우저 viewport
-    rootMargin: "1000px 0px 0px 0px", // rootMargin을 '10px 10px 10px 10px'로 설정
+    rootMargin: "100px 0px 0px 0px", // rootMargin을 '10px 10px 10px 10px'로 설정
     threshold: 1 // 타겟 엘리먼트가 교차영역에 진입했을 때, 교차영역에 타켓 엘리먼트의 50%가 있을 때, 교차 영역에 타켓 엘리먼트의 100%가 있을 때 observe가 반응한다.
   };
 
@@ -25,21 +25,20 @@ const MainPage = () => {
       if (entry.isIntersecting) {
         console.log(entry.target,'true');
         entry.target.classList.add("zoom-in");
-        observer.unobserve(entry.target);
+        // observer.unobserve(entry.target);
 
       } else {
         console.log(entry.target,'fail');
-        observer.unobserve(entry.target);
+        // observer.unobserve(entry.target);
 
 
               
-        // entry.target.classList.remove('MainPageLoginBt')
         entry.target.classList.remove("zoom-in");
       }
     });
   }, options);
 
-  const button: any = document.querySelectorAll(".MainPageContents1");
+  const button: any = document.querySelectorAll(".MainPageLoginBt");
 
   button.forEach((el: any) => {
     io.observe(el);
