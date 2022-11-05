bf2c96ec9874847969d9d7673cccb3814bc73988
gitkrak <dpemdnjem23@gmail.com>
2022-11-05 23:50:40 +0900

    react observe

--- client/src/Components/Main/MainPageLoginBt.tsx
+++ client/src/Components/Main/MainPageLoginBt.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import {
@@ -12,16 +12,87 @@ import {
 import "../../css/components/MainPage/MainPageLoginBt.css";
 import { faSnowflake } from "@fortawesome/free-regular-svg-icons";
 const MainPageLoginBt = () => {
+  const MainPageLoginBtRef = useRef(null)
+
+  const [isVisible, setIsVisible] = useState<boolean>(false)
+  let direction = '';
+  let prevYposition = 0;
+
+  const checkScrollDirection = (prevY:any) => {
+    if (window.scrollY === 0 && prevY === 0) {
+          return;
+      } else if (window.scrollY > prevY) {
+          direction = 'down';
+      } else {
+          direction = 'up';
+      }
+
+      prevYposition = window.scrollY;
+  };
+
+
+
+  const callbackFunction = (entries:any) =>{
+
+    const[entry] =entries
+    console.log(entry)
+    console.log(entry.isIntersecting)
+    setIsVisible(entry.isIntersecting)
+
+
+  }
+
+    const options = {
+      root: null, // .container class를 가진 엘리먼트를 root로 설정. null일 경우 브라우저 viewport
+      rootMargin: "50px 0px 0px 0px", // rootMargin을 '10px 10px 10px 10px'로 설정
+      threshold: 1, // 타겟 엘리먼트가 교차영역에 진입했을 때, 교차영역에 타켓 엘리먼트의 50%가 있을 때, 교차 영역에 타켓 엘리먼트의 100%가 있을 때 observe가 반응한다.
+    };
+
+    useEffect(()=>{
+
+      const io = new IntersectionObserver(callbackFunction, options)
+      
+      if(MainPageLoginBtRef.current) {
+        console.log(MainPageLoginBtRef.current)
+
+        
+        
+        io.observe(MainPageLoginBtRef.current)
+
+      }
+      return()=>{
+        if(MainPageLoginBtRef.current) io.unobserve(MainPageLoginBtRef.current)
+
+      }
+      
+
+
+    },[MainPageLoginBtRef,options])
+  
+      // entry와 observer 출력
+   
+    // const button: any = document.querySelectorAll(".MainPageLoginBt");
+  
+    // button.forEach((el: any) => {
+    //   io.observe(el);
+    // });
+    const visible = isVisible ? 'zoom-in' :''
+  
+
+
+
 
   // io.observe(button)
 
   // io.observe(button);
   return (
-    <div className="MainPageLoginBt">
+    
+    <div ref={MainPageLoginBtRef} className={`MainPageLoginBt ${visible}`}>
       <Link to="/login">
         <FontAwesomeIcon icon={faSnowflake} /> 섭개더 시작하기
       </Link>
     </div>
+    
   );
 };
 
--- client/src/Pages/MainPage.tsx
+++ client/src/Pages/MainPage.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import {Routes, BrowserRouter, Link } from "react-router-dom";
+import { Routes, BrowserRouter, Link } from "react-router-dom";
 import MainPageContents1 from "Components/Main/MainPageContents1";
 import MainPageSlickList from "Components/Main/MainPageSlickList";
 import BottomBar from "Components/Common/footer";
 
-import '../css/pages/MainPage.css'
+import "../css/pages/MainPage.css";
 import MainPageLoginBt from "Components/Main/MainPageLoginBt";
 import MainPageContents2 from "Components/Main/MainPageContents2";
 import MainPageBoundary from "Components/Main/MainPageBoundary";
@@ -12,55 +12,20 @@ import MainPageContents3 from "Components/Main/MainPageContents3";
 import MainPageContents4 from "Components/Main/MainPageContents4";
 const MainPage = () => {
 
-
-  const options = {
-    root: null, // .container class를 가진 엘리먼트를 root로 설정. null일 경우 브라우저 viewport
-    rootMargin: "100px 0px 0px 0px", // rootMargin을 '10px 10px 10px 10px'로 설정
-    threshold: 1 // 타겟 엘리먼트가 교차영역에 진입했을 때, 교차영역에 타켓 엘리먼트의 50%가 있을 때, 교차 영역에 타켓 엘리먼트의 100%가 있을 때 observe가 반응한다.
-  };
-
-  const io = new IntersectionObserver((entries, observer) => {
-    // entry와 observer 출력
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        console.log(entry.target,'true');
-        entry.target.classList.add("zoom-in");
-        // observer.unobserve(entry.target);
-
-      } else {
-        console.log(entry.target,'fail');
-        // observer.unobserve(entry.target);
-
-
-              
-        entry.target.classList.remove("zoom-in");
-      }
-    });
-  }, options);
-
-  const button: any = document.querySelectorAll(".MainPageLoginBt");
-
-  button.forEach((el: any) => {
-    io.observe(el);
-  });
-
   return (
+    <div className="MainPage">
+      <MainPageContents1></MainPageContents1>
+      <MainPageSlickList></MainPageSlickList>
+      <MainPageContents2></MainPageContents2>
+      <MainPageBoundary></MainPageBoundary>
+      <MainPageContents3></MainPageContents3>
+      <MainPageBoundary></MainPageBoundary>
+      <MainPageContents4></MainPageContents4>
+      <MainPageLoginBt></MainPageLoginBt>
 
-    
-    <div className="App_MainContents">
-    <MainPageContents1></MainPageContents1>
-    <MainPageSlickList></MainPageSlickList>
-    <MainPageLoginBt></MainPageLoginBt>
-    <MainPageContents2></MainPageContents2>
-    <MainPageBoundary></MainPageBoundary>
-    <MainPageContents3></MainPageContents3>
-    <MainPageBoundary></MainPageBoundary>
-    <MainPageContents4></MainPageContents4>
-
-    <BottomBar></BottomBar>
-
-  </div>
-  )
+      <BottomBar></BottomBar>
+    </div>
+  );
 };
 
 export default MainPage;
