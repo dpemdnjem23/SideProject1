976918946e78c6a001c804f5e2fdf900265035af
gitkrak <dpemdnjem23@gmail.com>
2022-11-08 23:57:36 +0900

    수정

--- client/src/Components/Main/MainPageLoginBt.tsx
+++ client/src/Components/Main/MainPageLoginBt.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect,useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import {
@@ -12,77 +12,49 @@ import {
 import "../../css/components/MainPage/MainPageLoginBt.css";
 import { faSnowflake } from "@fortawesome/free-regular-svg-icons";
 const MainPageLoginBt = () => {
-  const MainPageLoginBtRef = useRef<HTMLDivElement>(null)
+  
+  const MainPageLoginBtRef = useRef<HTMLDivElement>(null);
 
-  const [isVisible, setIsVisible] = useState<boolean>(false)
-  let direction = '';
-  let prevYposition = 0;
+  const [isVisible, setIsVisible] = useState<boolean>(false);
 
-  const checkScrollDirection = (prevY:any) => {
-    if (window.scrollY === 0 && prevY === 0) {
-          return;
-      } else if (window.scrollY > prevY) {
-          direction = 'down';
-      } else {
-          direction = 'up';
-      }
-
-      prevYposition = window.scrollY;
+  const options = {
+    root: document.querySelector(".MainPageContents1"), // .container class를 가진 엘리먼트를 root로 설정. null일 경우 브라우저 viewport
+    rootMargin: "300px 0px 0px 0px", // rootMargin을 '10px 10px 10px 10px'로 설정
+    threshold: 1, // 타겟 엘리먼트가 교차영역에 진입했을 때, 교차영역에 타켓 엘리먼트의 50%가 있을 때, 교차 영역에 타켓 엘리먼트의 100%가 있을 때 observe가 반응한다.
   };
 
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
 
-    const options = {
-      root: document.querySelector('.MainPageContents1'), // .container class를 가진 엘리먼트를 root로 설정. null일 경우 브라우저 viewport
-      rootMargin: "50px 0px 0px 0px", // rootMargin을 '10px 10px 10px 10px'로 설정
-      threshold: 1, // 타겟 엘리먼트가 교차영역에 진입했을 때, 교차영역에 타켓 엘리먼트의 50%가 있을 때, 교차 영역에 타켓 엘리먼트의 100%가 있을 때 observe가 반응한다.
-    };
-
-    useEffect(()=>{
-
-      const io = new IntersectionObserver(
-        entries =>{
-          entries.forEach((entry)=>{
-            checkScrollDirection(prevYposition);
-            
-            if (entry.isIntersecting){
-      console.log('true')
-               setIsVisible(true);
-         }
-          })
-
-        }
-        
-        
-        , options)
+        console.log(entry) // entry is 'IntersectionObserverEntry'
+      })
 
 
-
-    },[MainPageLoginBtRef,options])
-  
-      // entry와 observer 출력
-   
-    // const button: any = document.querySelectorAll(".MainPageLoginBt");
-  
-    // button.forEach((el: any) => {
-    //   io.observe(el);
-    // });
-    const visible = isVisible ? 'zoom-in' :''
-  
+    }, options)
 
 
+    observer.observe(MainPageLoginBtRef);
 
 
-  // io.observe(button)
+  }, [MainPageLoginBtRef, options]);
+
+  // entry와 observer 출력
+
+  // const button: any = document.querySelectorAll(".MainPageLoginBt");
+
+  // button.forEach((el: any) => {
+  //   io.observe(el);
+  // });
+  const visible = isVisible ? "zoom-in" : "";
 
   // io.observe(button);
   return (
-
     <div ref={MainPageLoginBtRef} className={`MainPageLoginBt ${visible}`}>
       <Link to="/login">
         <FontAwesomeIcon icon={faSnowflake} /> 섭개더 시작하기
       </Link>
     </div>
-    
   );
 };
 
