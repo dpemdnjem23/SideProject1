5747be89d2659bdd862e62544628925586671024
gitkrak <dpemdnjem23@gmail.com>
2022-11-12 23:51:22 +0900

    text

--- client/src/Components/Main/MainPageContents2.tsx
+++ client/src/Components/Main/MainPageContents2.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-// import 
+// import
 import {
   Route,
   BrowserRouter,
@@ -9,16 +9,34 @@ import {
   useNavigate,
 } from "react-router-dom";
 
-import "../../css/components/MainPage/MainPageContents2.css"
-const MainPageContents2 = () =>{
+import "../../css/components/MainPage/MainPageContents2.css";
+const MainPageContents2 = () => {
+  return (
+    <div className="MainPageContents2">
+      <div className="MainPageContents2_section">
+        <div className="MainPageContents2_section_text">
 
+<p className='MainPageContents2_section_text_category'>
+  SUBSCRIPTION
+  
+</p>
+        <h2 className='MainPageContents2_section_text_title'>
+          보다 편하게 관리<br></br>구독관리
 
+        </h2>
+        <p>
+          사용하고이고 고
 
-    return (
-        <div className="MainPageContents2">
-
+        </p>
         </div>
-    )
-}
+          <div className="MainPageContents2_section_image">
 
-export default MainPageContents2
+          </div>
+
+        
+      </div>
+    </div>
+  );
+};
+
+export default MainPageContents2;
