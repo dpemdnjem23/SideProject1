b4736d80520394c49246f5630d72283382229bc0
gitkrak <dpemdnjem23@gmail.com>
2022-11-26 23:54:49 +0900

    css수정

--- client/src/Components/Main/MainPageContents2.tsx
+++ client/src/Components/Main/MainPageContents2.tsx
@@ -53,16 +53,16 @@ const MainPageContents2 = () => {
       <div  className="MainPageContents2_section">
         <div ref={target2} className={`MainPageContents2_section_text ${on}`}>
           {/* <div {...animatedItem1}> */}
-            <p className="raise_up1">
+            <p className="raise_up1 MainPageContents2_section_text_category">
               SUBSCRIPTION
             </p>
-            <h2 className="MainPageContents2_section_text_title raise_up1">
+            <h2 className="raise_up1 MainPageContents2_section_text_title">
               보다 편하게 관리<br></br>구독관리
             </h2>
           {/* </div> */}
 
           {/* <div {...animatedItem2}> */}
-            <p className="MainPageContents2_section_text_contents MainPageContents raise_up2">
+            <p className="raise_up2 MainPageContents2_section_text_contents MainPageContents">
               사용하고 있는 구독명을 선택하고, <br></br> 기간, 가격을 입력하고
               등록해주세요
             </p>
