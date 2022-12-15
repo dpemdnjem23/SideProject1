f57fdc993a47e190da09761748ce76b32545ce20
gitkrak <dpemdnjem23@gmail.com>
2022-12-15 23:17:19 +0900

    수정

--- client/src/Components/Main/MainPageContents5.tsx
+++ client/src/Components/Main/MainPageContents5.tsx
@@ -17,6 +17,18 @@ const MainPageContents5 = () =>{
 
     return (
         <div className="MainPageContents5">
+          <div className="MainPageContents5_section">
+            <h1 className="MainPageContents5_text">
+              <div className='arrow_box'></div>
+              <span>누가</span>
+              <span>저렴하고,</span>
+              <span>더</span>
+              <span>안전하게</span>
+              <div>
+                OTT를 즐겨보세요
+              </div>
+            </h1>
+          </div>
 
         </div>
     )
