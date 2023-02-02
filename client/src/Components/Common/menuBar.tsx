5e91ebb281df4c2cbe8d3176d55fe9f487426899
gitkrak <dpemdnjem23@gmail.com>
2023-02-02 23:47:47 +0900

    수정

--- client/src/Components/Common/menuBar.tsx
+++ client/src/Components/Common/menuBar.tsx
@@ -14,13 +14,41 @@ import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 import "../../css/common/menuBar.css";
 import XButton from "./XButton";
+import {
+  mainheaderuseStore,
+  showErrModalState,
+  useWalletStore,
+} from "utils/state";
 
 const MenuBar = () => {
+  const { showMypageModal, mobileMyPageOn, mobileMyPage, showMypageModalOn } =
+    mainheaderuseStore();
+  const { setShowErrModal } = showErrModalState();
+  const { setShowSubEdit, setShowSubDetail } = useWalletStore();
+
+  const closeMenuBar = () => {
+    mobileMyPageOn(false);
+    console.log("asd");
+  };
+  const navigate = useNavigate();
+
+  const handleErrModal = () => {
+    setShowSubDetail(false);
+    setShowSubEdit(false);
+    mobileMyPageOn(false)
+    if (localStorage.getItem("accessToken")) {
+      setShowErrModal(false);
+    } else {
+      navigate("/");
+      setShowErrModal(true);
+    }
+  };
+
   return (
-    <div id="menuBar">
+    <div onClick={(e) => e.stopPropagation()} id="menuBar">
       <div className="menuBarSection">
-        <div className='menuBar_Xbutton'>
-          <div>x</div>
+        <div className="menuBar_Xbutton">
+          <button onClick={closeMenuBar}>x</button>
         </div>
         <div className="menuBarSection_top">
           <img className="menuBarimg" src="/images/2.png"></img>
@@ -30,48 +58,49 @@ const MenuBar = () => {
         </div>
 
         <div className="menuBarSection_middle">
-          <div>
-            <Link to="/login"> 로그인</Link>
-          </div>
+          <div className="menuBarSection_down_main">메인메뉴</div>
         </div>
 
         <div className="menuBarSection_down">
-          <div className="menuBarSection_down_main">메인메뉴</div>
           <ul className="menuBarSection_main">
             <li>
-            <div>
+              <div>
+                <Link to="/">메인페이지</Link>
 
-              <span>메인페이지</span>
-
-            
-              <FontAwesomeIcon className='right_angle' icon={faChevronRight}></FontAwesomeIcon>
+                <FontAwesomeIcon
+                  className="right_angle"
+                  icon={faChevronRight}
+                ></FontAwesomeIcon>
               </div>
- 
+            </li>
+            <li>
+              <div onClick={handleErrModal}>
+                <Link to="/wallet">구독지갑</Link>
+
+                <FontAwesomeIcon
+                  className="right_angle"
+                  icon={faChevronRight}
+                ></FontAwesomeIcon>
+              </div>
             </li>
             <li>
               <div>
+                <Link to={`/share`}>구독공유</Link>
 
-              <span>구독지갑</span>
-
-              <FontAwesomeIcon className='right_angle'  icon={faChevronRight}></FontAwesomeIcon>
+                <FontAwesomeIcon
+                  className="right_angle"
+                  icon={faChevronRight}
+                ></FontAwesomeIcon>
               </div>
-
             </li>
             <li>
-            <div>
+              <div onClick={handleErrModal}>
+                <Link to="/callendar">구독달력</Link>
 
-              <span>구독공유</span>
-
-              <FontAwesomeIcon className='right_angle' icon={faChevronRight}></FontAwesomeIcon>
-              </div>
-
-            </li>
-            <li>
-              <div>
-
-              <span>구독달력</span>
-
-              <FontAwesomeIcon className='right_angle' icon={faChevronRight}></FontAwesomeIcon>
+                <FontAwesomeIcon
+                  className="right_angle"
+                  icon={faChevronRight}
+                ></FontAwesomeIcon>
               </div>
             </li>
             <li></li>
