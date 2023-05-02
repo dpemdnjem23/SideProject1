971b71b9d5260dff61d789da3ac33a8373150f8c
gitkrak <dpemdnjem23@gmail.com>
2023-05-02 23:54:57 +0900

    수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -67,7 +67,10 @@ import AlarmPage from "Pages/AlarmPage";
 //   setTimeIsNow: (input: number) => void;
 // };
 
+
+
 const cancelTokenSource = axios.CancelToken.source();
+
 
 export const instance: AxiosInstance = axios.create({
   baseURL: `${process.env.REACT_APP_API_URI}`,
@@ -84,6 +87,10 @@ axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.headers.get["Content-Type"] = "application/json";
 
 const App = () => {
+
+
+
+
   // const navigate = useNavigate();
   // const navigate = useNavigate()
   //!
@@ -164,136 +171,135 @@ const App = () => {
   // useAxiosInterceptors()
 
   // useEffect(() => {
-    const requestInstance: any = instance.interceptors.request.use(
-      async (config: AxiosRequestConfig) => {
-        const accessToken: string | null = localStorage.getItem("accessToken");
+  const requestInstance: any = instance.interceptors.request.use(
+    async (config: AxiosRequestConfig) => {
+      const accessToken: string | null = localStorage.getItem("accessToken");
 
-        config.headers = {
-          Authorization: `Bearer ${accessToken}`,
-        };
-        if (!accessToken) {
-          console.log("존재하지않아");
-          if (config.url === "/alarm/info") {
-            instance.interceptors.request.eject(requestInstance);
-            instance.interceptors.response.eject(responseInstance);
-            return;
-          }
+      config.headers = {
+        Authorization: `Bearer ${accessToken}`,
+      };
+      if (!accessToken) {
+        console.log("존재하지않아");
+        if (config.url === "/alarm/info") {
+          instance.interceptors.request.eject(requestInstance);
+          instance.interceptors.response.eject(responseInstance);
+          return;
         }
-
-        if (accessToken) {
-          // accessToken이 있는 경우, 요청 헤더에 추가합니다.
-
-          if (config.url === "/auth/signout") {
-            instance.interceptors.request.eject(requestInstance);
-            instance.interceptors.response.eject(responseInstance);
-            return config;
-          }
-        }
-
-        return config;
-      },
-      async (error) => {
-        return Promise.reject(error);
       }
-    );
 
-    const responseInstance = instance.interceptors.response.use(
-      (response) => {
-        return response;
-      },
-      async (error) => {
-        const originalRequest = error.config;
+      if (accessToken) {
+        // accessToken이 있는 경우, 요청 헤더에 추가합니다.
 
-        // console.log(
-        //   originalRequest._retry,
-        //   localstorageUserInfo.accessExp < today
-        // );
+        if (config.url === "/auth/signout") {
+          instance.interceptors.request.eject(requestInstance);
+          instance.interceptors.response.eject(responseInstance);
+          return config;
+        }
+      }
 
-        if (!originalRequest._retry && localstorageUserInfo.accessExp < today) {
-          originalRequest._retry = true;
+      return config;
+    },
+    async (error) => {
+      return Promise.reject(error);
+    }
+  );
 
-          return axios
-            .post(
-              `${process.env.REACT_APP_API_URI}/auth/issueaccess`,
-              {
-                id: localstorageUserInfo.id,
+  const responseInstance = instance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    async (error) => {
+      const originalRequest = error.config;
+
+      // console.log(
+      //   originalRequest._retry,
+      //   localstorageUserInfo.accessExp < today
+      // );
+
+      if (!originalRequest._retry && localstorageUserInfo.accessExp < today) {
+        originalRequest._retry = true;
+
+        return axios
+          .post(
+            `${process.env.REACT_APP_API_URI}/auth/issueaccess`,
+            {
+              id: localstorageUserInfo.id,
+            },
+            {
+              headers: {
+                authorization: `Bearer ${accessToken}`,
               },
-              {
+            }
+          )
+          .then((res) => {
+            console.log("일로와");
+            localStorage.setItem("accessToken", res.data.accessToken);
+            //         //res.data
+            localStorage.setItem(
+              "subgatherUserInfo",
+              JSON.stringify(res.data.data)
+            );
+
+            axios.defaults.headers.common[
+              "Authorization"
+            ] = `Bearer ${res.data.accessToken}`;
+
+            // return axios.request(originalRequest);
+
+            //  axios.request(originalRequest);
+
+            // setTokenExpired(result.accessToken);
+            // return instance(originalRequest);
+
+            //다시 요청
+          })
+          .catch((err) => {
+            //refreshToken이 만료가된경우 로그아웃을 한다 -> 만료
+
+            return axios
+              .get(`${process.env.REACT_APP_API_URI}/auth/signout`, {
                 headers: {
                   authorization: `Bearer ${accessToken}`,
                 },
-              }
-            )
-            .then((res) => {
-              console.log("일로와");
-              localStorage.setItem("accessToken", res.data.accessToken);
-              //         //res.data
-              localStorage.setItem(
-                "subgatherUserInfo",
-                JSON.stringify(res.data.data)
-              );
+              })
 
-              axios.defaults.headers.common[
-                "Authorization"
-              ] = `Bearer ${res.data.accessToken}`;
+              .then((res) => {
+                //리프레쉬 토큰이 없는경우 로그아웃을 해야한다.
+                // window.location.replace("/");
 
-              return axios.request(originalRequest);
+                persistLogin(false);
+                console.log("tjfps");
 
-              //  axios.request(originalRequest);
+                window.alert("로그인이 만료되었습니다. 다시 로그인해주세요");
+                localStorage.clear();
+                isSigninState.persist.clearStorage();
 
-              // setTokenExpired(result.accessToken);
-              // return instance(originalRequest);
+                // cancelTokenSource.cancel();
+                // return Promise.reject(error);
+              })
+              .catch((err) => {
+                console.error(err);
 
-              //다시 요청
-            })
-            .catch((err) => {
-              //refreshToken이 만료가된경우 로그아웃을 한다 -> 만료
+                persistLogin(false);
+                // showMypageModalOn(false);
 
-              return axios
-                .get(`${process.env.REACT_APP_API_URI}/auth/signout`, {
-                  headers: {
-                    authorization: `Bearer ${accessToken}`,
-                  },
-                })
-
-                .then((res) => {
-                  //리프레쉬 토큰이 없는경우 로그아웃을 해야한다.
-                  // window.location.replace("/");
-                  navigate("/");
-
-                  persistLogin(false);
-                  console.log("tjfps");
-
-                  window.alert("로그인이 만료되었습니다. 다시 로그인해주세요");
-                  localStorage.clear();
-                  isSigninState.persist.clearStorage();
-
-                  // cancelTokenSource.cancel();
-                  // return Promise.reject(error);
-                })
-                .catch((err) => {
-                  console.error(err);
-
-                  persistLogin(false);
-                  // showMypageModalOn(false);
-
-                  localStorage.removeItem("accessToken");
-                  isSigninState.persist.clearStorage();
-                  localStorage.removeItem("subgatherUserInfo");
-                });
-            });
-        }
-        // }
-
-        //에러로 내보낸다.
-        return Promise.reject(error);
+                localStorage.removeItem("accessToken");
+                isSigninState.persist.clearStorage();
+                localStorage.removeItem("subgatherUserInfo");
+              });
+          });
       }
-    );
+      // }
 
-    // return () => {
-    //   instance.interceptors.request.eject(requestInstance);
-    //   instance.interceptors.request.eject(responseInstance);
-    // };
+      //에러로 내보낸다.
+      return Promise.reject(error);
+    }
+  );
+
+  // return () => {
+  //   instance.interceptors.request.eject(requestInstance);
+  //   instance.interceptors.request.eject(responseInstance);
+  // };
   // }, [instance]);
 
   useEffect(() => {
@@ -310,7 +316,6 @@ const App = () => {
       }
     };
     fetchData();
-
   }, [userSignin]);
   // useEffect(() => {
   //   return () => {
@@ -373,4 +378,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default App
